58fe2fae59321a18db71abe8721468c68e32c06c
valtraas <neptscode@gmail.com>
2025-12-07 20:52:57 +0700

    Merapikan tampilan dan menambahkan modal untuk image serta perhitungan uang

--- resources/views/transaksi/transaksi.blade.php
+++ resources/views/transaksi/transaksi.blade.php
@@ -3,8 +3,18 @@
 <a href="{{ route('transaksi.create') }}"
     class="block w-1/4  text-center px-4 py-2 border-blue-500 border my-5 rounded-md transition-all duration-300 hover:shadow-md shadow-blue-500/60 hover:bg-blue-500 hover:text-white hover:-translate-y-1 hover:font-bold">Tambah
     Transaksi</a>
-<div>
-    <table class="table-auto table">
+
+    <div class="mt-10">
+        <form action="" id="searchForm">
+            <div class="border rounded-xl w-1/4 flex justify-between items-center px-2 ">
+                <i class="fa-solid fa-magnifying-glass block"></i>
+                <input type="text" class="w-full px-2 py-1  focus:outline-none rounded-md block" placeholder="Cari No Transaksi"
+                    autofocus>
+            </div>
+        </form>
+    </div>
+<div class="mt-4">
+    <table class="table-auto ">
         <thead>
             <tr>
                 <th class="border border-slate-700 p-4">No Transaksi</th>
@@ -39,9 +49,8 @@
                             class=" my-5 rounded-md px-2 py-1 block bg-yellow-500 text-white hover:scale-110 hover:shadow-md hover:shadow-yellow-500/60 transition-all duration-300"><i
                                 class="fa-solid fa-pen-to-square"></i></a>
                         <form action="" method="" class="deleteBtn cursor-pointer">
-
                             <button type="submit"
-                                class=" my-5 rounded-md px-2 py-1 block bg-red-600 text-white hover:scale-110 hover:shadow-md hover:shadow-red-600/60 transition-all duration-300"><i
+                                class="cursor-pointer my-5 rounded-md px-2 py-1 block bg-red-600 text-white hover:scale-110 hover:shadow-md hover:shadow-red-600/60 transition-all duration-300"><i
                                     class="fa-solid fa-trash-can"></i></button>
                         </form>
                     </div>
